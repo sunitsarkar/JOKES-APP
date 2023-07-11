6ac8434641ed6ad5f15116ed9fbf9fd457f03c56
sunit sarkar <sunit6503@gmail.com>
2023-07-11 11:00:25 +0530

    modified animation and some extra feature

--- src/bookmark.js
+++ src/bookmark.js
@@ -1,56 +1,70 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Emoji from "react-emoji-render";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
-export default function Bookmark(){
-    const data=JSON.parse(localStorage.getItem("bookmark"))
-    // console.log(data)
-    const navigate=useNavigate();
-    const [p,setP]=useState(true)
+export default function Bookmark() {
+  const data = JSON.parse(localStorage.getItem("bookmark"))
+  // console.log(data)
+  const navigate = useNavigate();
+  const [p, setP] = useState(true)
 
-    if(data){
-        return <div>
-        <motion.div
+  if (data) {
+    return <motion.div
+      initial={{ y: 1000 }}
+      animate={{ y: 0 }}
+      transition={{ type: "spring", duration: 1 }}>
+      <motion.div
         initial={{ y: 1000 }}
         animate={{ y: 0 }}
         transition={{ type: "spring", duration: 1 }}
       >
-         <div >
-        <div id='punbox'>
-          PUN<Emoji text=":smiley:" />: {<h3>{data.setup}</h3>}
-        </div>
-        <div id='linebox'>
-        Punchline<Emoji text=":laughing:" />: {<p>{data.punchline}</p>}
-        </div>
-      </div>
+        <motion.div 
+        initial={{ x: 1000 }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", duration: 1.5 }}
+        >
+          <div id='punbox'>
+            PUN<Emoji text=":smiley:" />: {<h3>{data.setup}</h3>}
+          </div>
+          <div id='linebox'>
+            Punchline<Emoji text=":laughing:" />: {<p>{data.punchline}</p>}
+          </div>
+        </motion.div>
       </motion.div>
-        <button className="btn" onClick={()=>{
-            localStorage.removeItem('bookmark');
-            setP(!p)
-        }}>clear</button>
-        <motion.button
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="btn" onClick={() => {
+          localStorage.removeItem('bookmark');
+          setP(!p)
+        }}>clear</motion.button>
+      <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         className="bookbtn"
-        onClick={()=>{navigate('/')}}
+        onClick={() => { navigate('/') }}
       >
         Go To Jokes Page
       </motion.button>
-    </div>
-    }
-    if(data=== null){
-        return <div>
-            <h1>no bookmarked joke</h1>
-            <motion.button
+    </motion.div>
+  }
+  if (data === null) {
+    return <motion.div
+      initial={{ y: 1000 }}
+      animate={{ y: 0 }}
+      transition={{ type: "spring", duration: 1 }}>
+      <h1>No bookmarked joke</h1>
+      <br/>
+      <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         className="bookbtn"
-        onClick={()=>{navigate('/')}}
+        onClick={() => { navigate('/') }}
       >
         Go To Jokes Page
       </motion.button>
-        </div>
-    }
+    </motion.div>
+  }
 }
--- src/jokes.js
+++ src/jokes.js
@@ -1,40 +1,51 @@
 import axios from 'axios';
 import './App.css';
 import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Emoji from 'react-emoji-render';
 import { useNavigate } from 'react-router-dom';
 
 export default function Jokes() {
-  const [data,setData]=useState();
-  const [temp,setTemp]=useState(true);
-  const navigate=useNavigate();
-  
+  const [data, setData] = useState();
+  const [temp, setTemp] = useState(true);
+  const navigate = useNavigate();
+  const [newjoke, setNewjoke] = useState(false);
 
-  const api="https://official-joke-api.appspot.com/random_joke";
-  useEffect(()=>{
-    axios.get(api).then((res)=>{
-    //   console.log(res.data);
+
+
+  const api = "https://official-joke-api.appspot.com/random_joke";
+  useEffect(() => {
+    axios.get(api).then((res) => {
+      //   console.log(res.data);
       setData(res.data);
     })
-  },[temp]);
+  }, [temp]);
   // console.log(data)
-//   const localStorage=window.localStorage;
-const handelbookmark=()=>{
+  //   const localStorage=window.localStorage;
+  const handelbookmark = () => {
     navigate('/bookmark')
-}
+  }
 
-const addBookmark=()=>{
-    window.localStorage.setItem(`bookmark`,JSON.stringify(data));
-}
-  
-  if(data){
+  const addBookmark = () => {
+    window.localStorage.setItem(`bookmark`, JSON.stringify(data));
+  }
+
+  const handelNewjoke = () => {
+    setTemp(!temp);
+    setNewjoke(true)
+    setTimeout(() => {
+      setNewjoke(false)
+    }, 1800);
+  };
+
+  if (data) {
     return (<div className="App">
       <motion.h1
         initial={{ y: -200 }}
         animate={{ y: 0 }}
         transition={{ type: "spring", duration: 0.5 }}
         whileHover={{ scale: 1.1 }}
+        id='title'
       >
         Jokes App
       </motion.h1>
@@ -43,39 +54,52 @@ const addBookmark=()=>{
         animate={{ y: 0 }}
         transition={{ type: "spring", duration: 1 }}
       >
-         <div >
-        <div id='punbox'>
-          PUN<Emoji text=":smiley:" />: {<h3>{data.setup}</h3>}
+        <div >
+          <motion.div
+            initial={{ x: 1000 }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", duration: 1.5 }}>
+            <div id='punbox'>
+              PUN<Emoji text=":smiley:" />: {<h3>{data.setup}</h3>}
+            </div>
+            <div id='linebox'>
+              Punchline<Emoji text=":laughing:" />: {<p>{data.punchline}</p>}
+            </div>
+          </motion.div>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="btn"
+            onClick={handelNewjoke}
+          >
+            New Joke
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="btn"
+            onClick={addBookmark}
+          >
+            Bookmark
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="bookbtn"
+            onClick={handelbookmark}
+          >
+            Go to Bookmark Page
+          </motion.button>
         </div>
-        <div id='linebox'>
-        Punchline<Emoji text=":laughing:" />: {<p>{data.punchline}</p>}
-        </div>
-        <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="btn"
-        onClick={()=>{setTemp(!temp)}}
-      >
-        New Joke
-      </motion.button>
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="btn"
-        onClick={addBookmark}
-      >
-        Bookmark
-      </motion.button>
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="bookbtn"
-        onClick={handelbookmark}
-      >
-        Go to Bookmark Page
-      </motion.button>
-      </div>
       </motion.div>
+      <br/><br/>
+      <AnimatePresence>
+        {newjoke && <motion.p
+          initial={{ y: -1000 }}
+          animate={{ y: 0 }}
+          exit={{ y: 100 }}
+        >Here is a new joke <Emoji text=":smiley:" /> </motion.p>}
+      </AnimatePresence>
     </div>
     );
   }
